6a7f5e0f1c2ee21af9c226a8bc7faadf8dec8208
Felix Grafschmidt <felix.grafschmidt@gmail.com>
2026-08-17 19:00:35 +0200

    list actual missions instead of filesize

--- euiv/missions/missions.ts
+++ euiv/missions/missions.ts
@@ -3,7 +3,7 @@ import klaw from 'klaw';
 
 const modDir = `/mnt/d/Games/Steam/steamapps/workshop/content/236850/`;
 
-const result: Array<{ size: number, filename: string, modname: string, gameversion: string }> = [];
+const result: Array<{ missionCount: number, filename: string, modname: string, gameversion: string }> = [];
 
 const filepath = 'euiv/missions/result.csv';
 
@@ -11,7 +11,7 @@ if (fs.existsSync(filepath)) {
   fs.unlinkSync(filepath);
 }
 
-fs.appendFileSync(filepath, 'modname,gameversion,filename,size\n');
+fs.appendFileSync(filepath, 'modname,gameversion,filename,missionCount\n');
 
 (async () => {
   const files = klaw(modDir, {
@@ -29,7 +29,7 @@ fs.appendFileSync(filepath, 'modname,gameversion,filename,size\n');
     if (modFilePathMatch) {
       let descriptor;
       if (!fs.existsSync(modFilePathMatch[0].concat('/descriptor.mod'))) {
-        // read other files with hte .mod extension
+        // read other files with the .mod extension
         const modFiles = fs.readdirSync(modFilePathMatch[0]);
         const modFile = modFiles.find(f => f.endsWith('.mod'));
         if (!modFile) {
@@ -58,13 +58,19 @@ fs.appendFileSync(filepath, 'modname,gameversion,filename,size\n');
     } else {
       console.error('Mod files outside mod folder', file.path);
     }
-    result.push({ size: file.stats.size, filename: file.path.split('/').reverse()[0], modname, gameversion });
+    const fileContent = fs.readFileSync(file.path).toString()
+    const missionCount = fileContent.match(/position =/g)?.length || 0;
+    if (missionCount === 0) {
+      console.log('No missions found in', file.path);
+      continue;
+    }
+    result.push({ missionCount, filename: file.path.split('/').reverse()[0], modname, gameversion });
   }
   result.sort((s1, s2) => {
-    return s1.size > s2.size ? -1 : 1;
+    return s1.missionCount > s2.missionCount ? -1 : 1;
   });
 
   result.forEach((e) => {
-    fs.appendFileSync(filepath, `"${e.modname}","${e.gameversion}","${e.filename}","${e.size}"\n`);
+    fs.appendFileSync(filepath, `"${e.modname}","${e.gameversion}","${e.filename}","${e.missionCount}"\n`);
   });
 })();
